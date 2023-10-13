0448cec9bcb0e3354145570111fc10818ca11dfc
Hrayr Avagyan <avagyanhro92@gmail.com>
2023-10-14 00:36:48 +0400

    running game improved

--- main.py
+++ main.py
@@ -12,31 +12,22 @@ play = input("you want to play against the computer?(Yes or No) ").lower()
 USER_VALUES = {'user1': get_value()}
 USER_VALUES['user2'] = 1 - USER_VALUES['user1']
 turn = 'user1' if USER_VALUES['user1'] == 1 else 'user2'
-if play == "no":
+show(grid)
+while True:
+    print(turn.upper(), 'turn')
+    pos = get_position(grid)
+    add(grid, USER_VALUES[turn], pos)
     show(grid)
-    while True:
-        print(turn.upper(), 'turn')
-        pos = get_position(grid)
-        add(grid, USER_VALUES[turn], pos)
+    status = is_game_over(grid)
+    if status:
+        print(turn.upper(), 'won')
+        break
+    if play == "yes":
+        computer_step(grid, USER_VALUES['user2'])
         show(grid)
         status = is_game_over(grid)
         if status:
             print(turn.upper(), 'won')
             break
-        turn = 'user1' if turn == 'user2' else 'user2'
-elif play == "yes":
-    show(grid)
-    while True:
-        print(turn.upper(),'turn')
-        if turn == 'user1':
-            pos = get_position(grid)
-            add(grid,USER_VALUES[turn],pos)
-        else:
-            computer_step(grid,USER_VALUES['user2'])
-        show(grid)
-        status = is_game_over(grid)
-        if status:
-            print(turn.upper(),'won')
-            break
-        turn = 'user1' if turn == 'user2' else 'user2'
-
+        continue
+    turn = 'user1' if turn == 'user2' else 'user2'
